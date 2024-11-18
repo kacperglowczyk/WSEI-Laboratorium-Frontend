e15c90e807b4f19e8468a02f38f724db7bc916be
kacperglowczyk <glowczykkacper@gmail.com>
2024-11-18 17:01:40 +0100

    updated structure

--- Lab01-02/src/main.jsx
+++ Lab01-02/src/main.jsx
@@ -1,7 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
-import App from './App';
+import { BrowserRouter } from 'react-router-dom'; // Import BrowserRouter
+import App from './App'; // Import the App component
+import './index.css'; // Import global styles (if any)
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
