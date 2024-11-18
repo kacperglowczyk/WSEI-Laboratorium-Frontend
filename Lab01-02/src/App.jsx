01f8370b2429d7802d3276ade5ab1a251fceb616
kacperglowczyk <glowczykkacper@gmail.com>
2024-11-18 17:16:16 +0100

    Lab3

--- Lab01-02/src/App.jsx
+++ Lab01-02/src/App.jsx
@@ -4,12 +4,14 @@ import RootLayout from './layouts/RootLayout';
 import Home from './pages/Home';
 import Lab1 from './pages/Lab1';
 import Lab2 from './pages/Lab2';
+import Lab3 from './pages/Lab3';
 import NotFound from './pages/NotFound';
 
 const menuItems = [
   { id: 1, label: 'Home', url: '/', urlPattern: '/', element: <Home /> },
   { id: 2, label: 'Laboratorium 1', url: '/lab1', urlPattern: '/lab1', element: <Lab1 /> },
   { id: 3, label: 'Laboratorium 2', url: '/lab2/:id', urlPattern: '/lab2/:id', element: <Lab2 /> },
+  { id: 4, label: 'Laboratorium 3', url: '/lab3', urlPattern: '/lab3', element: <Lab3 /> },
 ];
 
 function App() {
---
+++ Lab01-02/src/pages/Lab3.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Lab3() {
+  return <h1>Laboratorium 3</h1>;
+}
+
+export default Lab3;
